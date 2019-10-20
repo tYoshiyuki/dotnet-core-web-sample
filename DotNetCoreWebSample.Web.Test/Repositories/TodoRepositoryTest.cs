586a47be7d51985ae9edd775e7bc3d8f943bcfcd
yTsunomoto <wasiwasimaru@gmail.com>
2019-10-21 01:09:31 +0900

    update 更新。

--- DotNetCoreWebSample.Web.Test/Repositories/TodoRepositoryTest.cs
+++ DotNetCoreWebSample.Web.Test/Repositories/TodoRepositoryTest.cs
@@ -6,6 +6,7 @@ using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using Xunit;
 
 namespace DotNetCoreWebSample.Web.Test.Repositories
@@ -14,7 +15,6 @@ namespace DotNetCoreWebSample.Web.Test.Repositories
     {
         private readonly TodoRepository _repository;
         private readonly IQueryable<Todo> _mockData;
-        private readonly Mock<DotnetCoreWebSampleContext> _mockContext;
         private readonly Mock<DbSet<Todo>> _mockDbSet;
 
         public TodoRepositoryTest()
@@ -31,11 +31,13 @@ namespace DotNetCoreWebSample.Web.Test.Repositories
             _mockDbSet.As<IQueryable<Todo>>().Setup(m => m.Expression).Returns(_mockData.Expression);
             _mockDbSet.As<IQueryable<Todo>>().Setup(m => m.ElementType).Returns(_mockData.ElementType);
             _mockDbSet.As<IQueryable<Todo>>().Setup(m => m.GetEnumerator()).Returns(_mockData.GetEnumerator());
+            _mockDbSet.Setup(_ => _.Find(It.IsAny<object[]>()))
+                .Returns<object[]>(ids => _mockData.FirstOrDefault(_ => _.Id == (int) ids[0]));
 
-            _mockContext = new Mock<DotnetCoreWebSampleContext>();
-            _mockContext.Setup(_ => _.Todo).Returns(_mockDbSet.Object);
-            _mockContext.Setup(_ => _.Set<Todo>()).Returns(_mockDbSet.Object);
-            _repository = new TodoRepository(_mockContext.Object);
+            var mockContext = new Mock<DotnetCoreWebSampleContext>();
+            mockContext.Setup(_ => _.Todo).Returns(_mockDbSet.Object);
+            mockContext.Setup(_ => _.Set<Todo>()).Returns(_mockDbSet.Object);
+            _repository = new TodoRepository(mockContext.Object);
         }
 
         [Fact]
@@ -55,6 +57,21 @@ namespace DotNetCoreWebSample.Web.Test.Repositories
         }
 
         [Fact]
+        public void Get_条件有_正常系()
+        {
+            // Arrange
+            const int id = 1;
+            Expression<Func<Todo, bool>> condition = t => t.Id == id;
+
+            // Act
+            var result = _repository.Get(condition);
+
+            // Assert
+            result.Count.Is(1);
+            result.First().IsStructuralEqual(_mockData.First(_ => _.Id == id));
+        }
+
+        [Fact]
         public void Add_正常系()
         {
             // Arrange
@@ -68,5 +85,19 @@ namespace DotNetCoreWebSample.Web.Test.Repositories
                                                             && t.Description == expect.Description
                                                             && t.CreatedDate == expect.CreatedDate)), Times.Once);
         }
+
+        [Fact]
+        public void Find_正常系()
+        {
+            // Arrange
+            const int id = 1;
+
+            // Act
+            var result = _repository.Find(id);
+
+            // Assert
+            result.IsStructuralEqual(_mockData.First(_ => _.Id == id));
+        }
+
     }
 }
---
+++ DotNetCoreWebSample.Web.Test/Services/ToDoServiceTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChainingAssertion;
+using DotNetCoreWebSample.Web.Models;
+using DotNetCoreWebSample.Web.Repositories;
+using DotNetCoreWebSample.Web.Services;
+using Moq;
+using Xunit;
+
+namespace DotNetCoreWebSample.Web.Test.Services
+{
+    public class ToDoServiceTest
+    {
+        private readonly Mock<ITodoRepository> _mock;
+        private readonly ToDoService _service;
+
+        public ToDoServiceTest()
+        {
+            _mock = new Mock<ITodoRepository>();
+
+            var data = new List<Todo>
+            {
+                new Todo {Id = 1, Description = "Title 001", CreatedDate = DateTime.Now},
+                new Todo {Id = 2, Description = "Title 002", CreatedDate = DateTime.Now},
+                new Todo {Id = 3, Description = "Title 003", CreatedDate = DateTime.Now}
+            };
+
+            _mock.Setup(_ => _.FindAsync(It.IsAny<int>()))
+                .Returns<object>(t => Task.FromResult(data.First(d => d.Id == (int)t)));
+
+            _service = new ToDoService(_mock.Object);
+        }
+
+        [Fact]
+        public void Get_正常系()
+        {
+            // Arrange・Act
+            var result = _service.Get(1);
+
+            // Assert
+            _mock.Verify(_ => _.FindAsync(It.Is<int>(i => i == 1)), Times.Once());
+            result.Id.Is(1);
+        }
+
+        // TODO 別テストケースの作成
+    }
+}
