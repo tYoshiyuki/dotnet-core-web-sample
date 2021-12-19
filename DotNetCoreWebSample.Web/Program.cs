46cb5a64fe3e1271bb591359a55046f890b01e3d
Yoshiyuki Tsunomoto <wasiwasimaru@gmail.com>
2021-12-19 17:17:07 +0900

    update .NET5 から .NET6 に更新。

--- DotNetCoreWebSample.Web/Program.cs
+++ DotNetCoreWebSample.Web/Program.cs
@@ -1,39 +1,92 @@
-﻿using DotNetCoreWebSample.Web.Models;
-using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using DotNetCoreWebSample.Web.Models;
+using DotNetCoreWebSample.Web.Repositories;
+using DotNetCoreWebSample.Web.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
 
-namespace DotNetCoreWebSample.Web
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Host.ConfigureServices(services =>
 {
-    public class Program
+    services.AddSession(options =>
     {
-        public static void Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
-            using (var serviceScope = host.Services.CreateScope())
-            {
-                try
-                {
-                    var context = serviceScope.ServiceProvider.GetService<DotnetCoreWebSampleContext>();
-                    context.Database.Migrate();
-                }
-                catch (Exception ex)
-                {
-                    var logger = serviceScope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred initializing the DB.");
-                }
-            }
-            host.Run();
-        }
+        options.Cookie.Name = "Session";
+        options.IdleTimeout = TimeSpan.FromMinutes(60);
+    });
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    services.AddDistributedSqlServerCache(options =>
+    {
+        options.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        options.SchemaName = "dbo";
+        options.TableName = "Sessions";
+    });
+
+    services.Configure<CookiePolicyOptions>(options =>
+    {
+        // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+        options.CheckConsentNeeded = context => true;
+        options.MinimumSameSitePolicy = SameSiteMode.None;
+    });
+
+    services.AddDbContext<DotnetCoreWebSampleContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    services.AddTransient<IToDoService, ToDoService>();
+    services.AddTransient<ITodoRepository, TodoRepository>();
+
+    services.AddControllersWithViews(options => options.ModelBinderProviders.RemoveType<DateTimeModelBinderProvider>());
+    services.AddRazorPages();
+
+});
+
+var app = builder.Build();
+
+using var serviceScope = app.Services.CreateScope();
+try
+{
+    var context = serviceScope.ServiceProvider.GetService<DotnetCoreWebSampleContext>();
+    context.Database.Migrate();
 }
+catch (Exception ex)
+{
+    var logger = serviceScope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred initializing the DB.");
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+
+// app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseCors();
+
+app.UseCookiePolicy();
+app.UseSession();
+
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
