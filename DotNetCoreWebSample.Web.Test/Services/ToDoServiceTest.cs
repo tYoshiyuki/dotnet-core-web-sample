bc3819b55fdac911847dfe1aaa5bf8fe5d1f11cc
Yoshiyuki Tsunomoto <wasiwasimaru@gmail.com>
2023-03-29 22:24:35 +0900

    update テストを更新。

--- DotNetCoreWebSample.Web.Test/Services/ToDoServiceTest.cs
+++ DotNetCoreWebSample.Web.Test/Services/ToDoServiceTest.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ChainingAssertion;
 using DotNetCoreWebSample.Web.Models;
 using DotNetCoreWebSample.Web.Repositories;
@@ -16,20 +14,24 @@ namespace DotNetCoreWebSample.Web.Test.Services
     {
         private readonly Mock<ITodoRepository> _mock;
         private readonly ToDoService _service;
+        private readonly List<Todo> _data;
 
         public ToDoServiceTest()
         {
             _mock = new Mock<ITodoRepository>();
 
-            var data = new List<Todo>
+            _data = new List<Todo>
             {
-                new Todo {Id = 1, Description = "Title 001", CreatedDate = DateTime.Now},
-                new Todo {Id = 2, Description = "Title 002", CreatedDate = DateTime.Now},
-                new Todo {Id = 3, Description = "Title 003", CreatedDate = DateTime.Now}
+                new() {Id = 1, Description = "Title 001", CreatedDate = DateTime.Now},
+                new() {Id = 2, Description = "Title 002", CreatedDate = DateTime.Now},
+                new() {Id = 3, Description = "Title 003", CreatedDate = DateTime.Now}
             };
 
-            _mock.Setup(_ => _.FindAsync(It.IsAny<int>()))
-                .Returns<object>(t => Task.FromResult(data.First(d => d.Id == (int)t)));
+            _mock.Setup(x => x.FindAsync(It.IsAny<int>()))
+                .ReturnsAsync((int x) => _data.First(d => d.Id == x));
+
+            _mock.Setup(_ => _.GetAsync())
+                .ReturnsAsync(_data);
 
             _service = new ToDoService(_mock.Object);
         }
@@ -42,7 +44,20 @@ namespace DotNetCoreWebSample.Web.Test.Services
 
             // Assert
             _mock.Verify(_ => _.FindAsync(It.Is<int>(i => i == 1)), Times.Once());
-            result.Id.Is(1);
+
+            result.IsStructuralEqual(_data.First(x => x.Id == 1));
+        }
+
+        [Fact]
+        public void GetList_正常系()
+        {
+            // Arrange・Act
+            var result = _service.GetList();
+
+            // Assert
+            _mock.Verify(_ => _.GetAsync(), Times.Once());
+
+            result.IsStructuralEqual(_data);
         }
 
         // TODO 別テストケースの作成
